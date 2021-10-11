e2eb2a1519dba50c57a211f318aaf1e76fd95154
kushedow <Polemika2020>
2021-10-11 20:43:06 +0300

    initial

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+posts = [
+    {"name": "Василий", "content": "Это Василий"},
+    {"name": "Даша", "content": "Это Даша"},
+]
+
+
+@app.route('/')
+def index():
+    return render_template("list.html", posts=posts)
+
+@app.route('/api/', methods=["POST"])
+def get_json():
+    data = request.json.get("data")
+    print(data)
+    return jsonify({"name": "sveta"})
+
+app.run()
